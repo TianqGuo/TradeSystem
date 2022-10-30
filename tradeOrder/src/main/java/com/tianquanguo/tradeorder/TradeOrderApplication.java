fbe1dc657b08d500d4c7f3100760302a19cbe2eb
Tianquan Guo <tguo67@gatech.edu>
2022-10-29 21:23:50 -0700

    Added first commit and currently application is runnable.
    Port 8081

---
+++ tradeOrder/src/main/java/com/tianquanguo/tradeorder/TradeOrderApplication.java
@@ -0,0 +1,13 @@
+package com.tianquanguo.tradeorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeOrderApplication.class, args);
+    }
+
+}
