5ac8c666c869b8abeafb39b7a6e4e74e7ebb41e2
Tianquan Guo <tguo67@gatech.edu>
2022-12-04 14:20:22 -0800

    Added sec kill but it is functional now:
    Fixed the issue in the application.properties, added the * in the mybatis.mapper-locations.
    
    tradeGoods Port 8081
    
    tradeWebPortal Port 8082
    
    trade sec kill port 8083
    
    tradeOrder Port 8083
    
    tradeWebManager Port 8090

--- tradeLightningDeal/src/main/java/com/tianquan/trade/lightning/deal/db/dao/impl/SeckillActivityDaoImpl.java
+++ tradeLightningDeal/src/main/java/com/tianquan/trade/lightning/deal/db/dao/impl/SeckillActivityDaoImpl.java
@@ -3,11 +3,13 @@ package com.tianquan.trade.lightning.deal.db.dao.impl;
 import com.tianquan.trade.lightning.deal.db.dao.SeckillActivityDao;
 import com.tianquan.trade.lightning.deal.db.mappers.SeckillActivityMapper;
 import com.tianquan.trade.lightning.deal.db.model.SeckillActivity;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class SeckillActivityDaoImpl implements SeckillActivityDao {
 
