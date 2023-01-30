36d0846360cb1eb8eb1f45c3c747c3ad19af6d52
TianqGuo <tianquanguo@gmail.com>
2023-01-29 18:03:43 -0800

    Distributed reconfiguration all done. Note that web manager still rely on the jar files.
    tradeGoods Port 8081
    
    tradeWebPortal Port 8082
    
    trade sec kill port 8083
    
    tradeOrder Port 8084
    
    tradeWebManager Port 8090
    
    tradeCommon Port 8091

--- tradeWebManager/src/main/java/com/tianquan/trade/web/manager/client/OrderFeignClient.java
+++ tradeWebManager/src/main/java/com/tianquan/trade/web/manager/client/OrderFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 //FeignClient注解设置服务提供者名字，在consul注册中心注册的服务名字
-@FeignClient(name = "trade-order", contextId = "trade-web-manager")
+@FeignClient(name = "tradeOrder", contextId = "tradeWebManager")
 public interface OrderFeignClient {
 
     @RequestMapping("/order/createOrder")
--- tradeWebManager/src/main/java/com/tianquan/trade/web/manager/client/SeckillActivityFeignClient.java
+++ tradeWebManager/src/main/java/com/tianquan/trade/web/manager/client/SeckillActivityFeignClient.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 //FeignClient注解设置服务提供者名字，在consul注册中心注册的服务名字
-@FeignClient(name = "trade-lightning-deal", contextId = "trade-web-manager")
+@FeignClient(name = "tradeLightningDeal", contextId = "tradeWebManager")
 public interface SeckillActivityFeignClient {
 
     /**
